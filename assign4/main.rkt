b36ea5344155d48663908afaeb507ddee5a0870d
Yiping Wang <yipingwang@uvic.ca>
2021-02-27 15:41:58 -0800

    formatting

--- assign4/main.rkt
+++ assign4/main.rkt
@@ -32,11 +32,11 @@
    ; (epwr tests)
    (check-equal? (add-pointwise '(1 2 3) '(4 2)) (list 5 4 3) "add-pointwise test")
    (check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(0 5 10 15 20)) (list 1 7 13 19 25 6 7 8 9 10) "add-pointwise test ep1")
-	 (check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(-1 -2 -3 -4 -5 -6)) (list 0 0 0 0 0 0 7 8 9 10) "add-pointwise test ep2")
-	 (check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(-2 10 15 20)) (list -1 12 18 24 5 6 7 8 9 10) "add-pointwise test ep3")
-	 (check-equal? (add-pointwise '(0 5 10 15 20) '(-1 -2 -3 -4 -5 -6)) (list -1 3 7 11 15 -6) "add-pointwise test ep4")
-	 (check-equal? (add-pointwise '(0 5 10 15 20) '(-2 10 15 20)) (list -2 15 25 35 20) "add-pointwise test ep5")
-	 (check-equal? (add-pointwise '(-1 -2 -3 -4 -5 -6) '(-2 10 15 20)) (list -3 8 12 16 -5 -6) "add-pointwise test ep6")
+	(check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(-1 -2 -3 -4 -5 -6)) (list 0 0 0 0 0 0 7 8 9 10) "add-pointwise test ep2")
+	(check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(-2 10 15 20)) (list -1 12 18 24 5 6 7 8 9 10) "add-pointwise test ep3")
+	(check-equal? (add-pointwise '(0 5 10 15 20) '(-1 -2 -3 -4 -5 -6)) (list -1 3 7 11 15 -6) "add-pointwise test ep4")
+	(check-equal? (add-pointwise '(0 5 10 15 20) '(-2 10 15 20)) (list -2 15 25 35 20) "add-pointwise test ep5")
+	(check-equal? (add-pointwise '(-1 -2 -3 -4 -5 -6) '(-2 10 15 20)) (list -3 8 12 16 -5 -6) "add-pointwise test ep6")
    
    ;-------------------add-pointwise-lists tests------------------------------
 
@@ -46,11 +46,11 @@
                  
    ; (epwr tests)
    (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6))) '(0 5 10 15 20 0 7 8 9 10) "add-pointwise-list test ep1")
-	 (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-100 -200 300 400 600))) '(-102 -185 325 435 620 0 7 8 9 10) "add-pointwise-list test ep2")
-	 (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6))) '(-2 -4 -6 -8 -10 -12 7 8 9 10) "add-pointwise-list test ep3")
-	 (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10))) '(1 2 3 4 5 6 7 8 9 10) "add-pointwise-list test ep4")
-	 (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-2 10 15 20) (-100 -200 300 400 600))) '(-101 -188 318 424 605 6 7 8 9 10) "add-pointwise-list test ep5")
-	 (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-2 10 15 20))) '(-4 20 30 40 0 0 7 8 9 10) "add-pointwise-list test ep6")
+	(check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-100 -200 300 400 600))) '(-102 -185 325 435 620 0 7 8 9 10) "add-pointwise-list test ep2")
+	(check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6))) '(-2 -4 -6 -8 -10 -12 7 8 9 10) "add-pointwise-list test ep3")
+	(check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10))) '(1 2 3 4 5 6 7 8 9 10) "add-pointwise-list test ep4")
+	(check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-2 10 15 20) (-100 -200 300 400 600))) '(-101 -188 318 424 605 6 7 8 9 10) "add-pointwise-list test ep5")
+	(check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-2 10 15 20))) '(-4 20 30 40 0 0 7 8 9 10) "add-pointwise-list test ep6")
 
    ;-------------------add-pointwise-lists-2 tests------------------------------
    
@@ -60,11 +60,11 @@
                  
    ; (epwr tests) 
    (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6))) '(0 5 10 15 20 0 7 8 9 10) "add-pointwise-list test ep1")
-	 (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-100 -200 300 400 600))) '(-102 -185 325 435 620 0 7 8 9 10) "add-pointwise-list test ep2")
-	 (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6))) '(-2 -4 -6 -8 -10 -12 7 8 9 10) "add-pointwise-list test ep3")
-	 (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10))) '(1 2 3 4 5 6 7 8 9 10) "add-pointwise-list test ep4")
-	 (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-2 10 15 20) (-100 -200 300 400 600))) '(-101 -188 318 424 605 6 7 8 9 10) "add-pointwise-list test ep5")
-	 (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-2 10 15 20))) '(-4 20 30 40 0 0 7 8 9 10) "add-pointwise-list test ep6")
+	(check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-100 -200 300 400 600))) '(-102 -185 325 435 620 0 7 8 9 10) "add-pointwise-list test ep2")
+	(check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6))) '(-2 -4 -6 -8 -10 -12 7 8 9 10) "add-pointwise-list test ep3")
+	(check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10))) '(1 2 3 4 5 6 7 8 9 10) "add-pointwise-list test ep4")
+	(check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-2 10 15 20) (-100 -200 300 400 600))) '(-101 -188 318 424 605 6 7 8 9 10) "add-pointwise-list test ep5")
+	(check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-2 10 15 20))) '(-4 20 30 40 0 0 7 8 9 10) "add-pointwise-list test ep6")
 
    ;-------------------stream-for-n-steps tests------------------------------
 
