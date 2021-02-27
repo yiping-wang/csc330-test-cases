6b1a3ed14be9738b870c7ea8e74298b987840672
greay66 <71350123+greay66@users.noreply.github.com>
2021-02-27 10:27:43 -0800

    Update tests.rkt
    added tests comment and formatting

--- assign4/tests.rkt
+++ assign4/tests.rkt
@@ -1,14 +1,54 @@
 #lang racket
-; ref: https://docs.racket-lang.org/test-engine/index.html#%28part._top%29
 
 (require "functions.rkt")
 (require test-engine/racket-tests)
 
 #|Test project to define some tests outside the scope of main. There is a slew of test options
 in require test-engine/racket-tests that cannot work in tandem with rackunit. One such test will
-check for the proper error. feel free to play around with this for additional testing - Greay|#
+check for the proper error. feel free to play around with this for additional testing - Greay
+
+Note: this can be automated using just main.rkt, enable (require "tests.rkt") in main.rkt for this feature|#
+
+
+(create-stream squares using (lambda (x) (* x x)) 
+          starting at  5
+          with increment 2)
+
+;------------------------add-pointwise tests--------------------------
+
+;(Greay tests)
 
 (check-error (add-pointwise '(3 4) 3) "illegal parameter")
 (check-error (add-pointwise 3 '(3 4)) "illegal parameter")
+   
+;-------------------add-pointwise-lists tests------------------------------
+
+;(Greay tests)
+(check-error (add-pointwise-lists '((1 1) (2 2 2 2) 3 ())) "illegal parameter")
+
+;-------------------add-pointwise-lists-2 tests------------------------------
+   
+
+;-------------------stream-for-n-steps tests------------------------------
+
+
+;--------------------------fibo-stream tests------------------------------
+  
+
+;--------------------------filter-stream tests------------------------------
+  
+
+;-----------------------palyndromic-numbers tests---------------------------
+  
+
+;-------------------------------squares tests------------------------------
+  
+
+;--------------------------vector-assoc tests------------------------------
+   
+
+;--------------------------cached-assoc tests------------------------------
+   
 
 (test)
+(newline)
