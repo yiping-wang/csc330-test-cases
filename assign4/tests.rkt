8b0dabe4ff47334ee5d69e09f0d49e75ca32dde1
greay66 <71350123+greay66@users.noreply.github.com>
2021-02-26 22:44:03 -0800

    Add files via upload
    added tests.rkt see file for comment

---
+++ assign4/tests.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(require "functions.rkt")
+(require test-engine/racket-tests)
+
+#|Test project to define some tests outside the scope of main. There is a slew of test options
+in require test-engine/racket-tests that cannot work in tandem with rackunit. One such test will
+check for the proper error. feel free to play around with this for additional testing - Greay|#
+
+(check-error (add-pointwise '(3 4) 3) "illegal parameter")
+(check-error (add-pointwise 3 '(3 4)) "illegal parameter")
+
+(test)
