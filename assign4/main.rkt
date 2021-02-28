74f8632353de56fc583ec13fc66940757ccc9b45
greay66 <71350123+greay66@users.noreply.github.com>
2021-02-27 19:31:54 -0800

    Update main.rkt
    fixed some formatting, 'cause I'm anal

--- assign4/main.rkt
+++ assign4/main.rkt
@@ -3,7 +3,7 @@
 ;;
 (require "functions.rkt")
 
-;if using test.rkt as well, uncomment below and make sure tests.rkt is in the same directory
+;if using tests.rkt as well, uncomment below and make sure tests.rkt is in the same directory
 ;(require "tests.rkt")
 
 (require rackunit)
@@ -19,9 +19,7 @@
 
    ;------------------------add-pointwise tests--------------------------
    
-   (check-equal? (add-pointwise '(1 2 3) '(4 2))
-                 (list 5 4 3)
-                 "add-pointwise test")
+   (check-equal? (add-pointwise '(1 2 3) '(4 2)) (list 5 4 3) "add-pointwise test")
    ;(Greay tests)
    (check-equal? (add-pointwise '() '()) null "add-pointwise test")
    (check-equal? (add-pointwise '(1 2) '(4 2 3)) (list 5 4 3) "add-pointwise test")
@@ -32,11 +30,11 @@
    ; (epwr tests)
    (check-equal? (add-pointwise '(1 2 3) '(4 2)) (list 5 4 3) "add-pointwise test")
    (check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(0 5 10 15 20)) (list 1 7 13 19 25 6 7 8 9 10) "add-pointwise test ep1")
-	(check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(-1 -2 -3 -4 -5 -6)) (list 0 0 0 0 0 0 7 8 9 10) "add-pointwise test ep2")
-	(check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(-2 10 15 20)) (list -1 12 18 24 5 6 7 8 9 10) "add-pointwise test ep3")
-	(check-equal? (add-pointwise '(0 5 10 15 20) '(-1 -2 -3 -4 -5 -6)) (list -1 3 7 11 15 -6) "add-pointwise test ep4")
-	(check-equal? (add-pointwise '(0 5 10 15 20) '(-2 10 15 20)) (list -2 15 25 35 20) "add-pointwise test ep5")
-	(check-equal? (add-pointwise '(-1 -2 -3 -4 -5 -6) '(-2 10 15 20)) (list -3 8 12 16 -5 -6) "add-pointwise test ep6")
+   (check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(-1 -2 -3 -4 -5 -6)) (list 0 0 0 0 0 0 7 8 9 10) "add-pointwise test ep2")
+   (check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(-2 10 15 20)) (list -1 12 18 24 5 6 7 8 9 10) "add-pointwise test ep3")
+   (check-equal? (add-pointwise '(0 5 10 15 20) '(-1 -2 -3 -4 -5 -6)) (list -1 3 7 11 15 -6) "add-pointwise test ep4")
+   (check-equal? (add-pointwise '(0 5 10 15 20) '(-2 10 15 20)) (list -2 15 25 35 20) "add-pointwise test ep5")
+   (check-equal? (add-pointwise '(-1 -2 -3 -4 -5 -6) '(-2 10 15 20)) (list -3 8 12 16 -5 -6) "add-pointwise test ep6")
    
    ; (vicky tests) NOTE: these can't check your message though
    (check-equal? (with-handlers ([exn:fail? (lambda (exn) 'ok)]) (add-pointwise "hi" '(1 2))) 'ok "add-pointwise test")
@@ -45,17 +43,15 @@
 
    ;-------------------add-pointwise-lists tests------------------------------
 
-   (check-equal? (add-pointwise-lists '((1 1) (2 2 2 2) (3) ()))
-                 '(6 3 2 2)
-                 "add-pointwise-list test")
+   (check-equal? (add-pointwise-lists '((1 1) (2 2 2 2) (3) ())) '(6 3 2 2) "add-pointwise-list test")
                  
    ; (epwr tests)
    (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6))) '(0 5 10 15 20 0 7 8 9 10) "add-pointwise-list test ep1")
-	(check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-100 -200 300 400 600))) '(-102 -185 325 435 620 0 7 8 9 10) "add-pointwise-list test ep2")
-	(check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6))) '(-2 -4 -6 -8 -10 -12 7 8 9 10) "add-pointwise-list test ep3")
-	(check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10))) '(1 2 3 4 5 6 7 8 9 10) "add-pointwise-list test ep4")
-	(check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-2 10 15 20) (-100 -200 300 400 600))) '(-101 -188 318 424 605 6 7 8 9 10) "add-pointwise-list test ep5")
-	(check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-2 10 15 20))) '(-4 20 30 40 0 0 7 8 9 10) "add-pointwise-list test ep6")
+   (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-100 -200 300 400 600))) '(-102 -185 325 435 620 0 7 8 9 10) "add-pointwise-list test ep2")
+   (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6))) '(-2 -4 -6 -8 -10 -12 7 8 9 10) "add-pointwise-list test ep3")
+   (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10))) '(1 2 3 4 5 6 7 8 9 10) "add-pointwise-list test ep4")
+   (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-2 10 15 20) (-100 -200 300 400 600))) '(-101 -188 318 424 605 6 7 8 9 10) "add-pointwise-list test ep5")
+   (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-2 10 15 20))) '(-4 20 30 40 0 0 7 8 9 10) "add-pointwise-list test ep6")
 
    ; (vicky tests)
    (check-equal? (add-pointwise-lists '(())) '() "add-pointwise-list test")
@@ -66,23 +62,19 @@
 
    ;-------------------add-pointwise-lists-2 tests------------------------------
    
-   (check-equal? (add-pointwise-lists-2 '((1 1) (2 2 2 2) (3) ()))
-                 '(6 3 2 2)
-                 "add-pointwise-list test")
+   (check-equal? (add-pointwise-lists-2 '((1 1) (2 2 2 2) (3) ())) '(6 3 2 2) "add-pointwise-list test")
                  
    ; (epwr tests) 
    (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6))) '(0 5 10 15 20 0 7 8 9 10) "add-pointwise-list test ep1")
-	(check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-100 -200 300 400 600))) '(-102 -185 325 435 620 0 7 8 9 10) "add-pointwise-list test ep2")
-	(check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6))) '(-2 -4 -6 -8 -10 -12 7 8 9 10) "add-pointwise-list test ep3")
-	(check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10))) '(1 2 3 4 5 6 7 8 9 10) "add-pointwise-list test ep4")
-	(check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-2 10 15 20) (-100 -200 300 400 600))) '(-101 -188 318 424 605 6 7 8 9 10) "add-pointwise-list test ep5")
-	(check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-2 10 15 20))) '(-4 20 30 40 0 0 7 8 9 10) "add-pointwise-list test ep6")
+   (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-100 -200 300 400 600))) '(-102 -185 325 435 620 0 7 8 9 10) "add-pointwise-list test ep2")
+   (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6))) '(-2 -4 -6 -8 -10 -12 7 8 9 10) "add-pointwise-list test ep3")
+   (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10))) '(1 2 3 4 5 6 7 8 9 10) "add-pointwise-list test ep4")
+   (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-2 10 15 20) (-100 -200 300 400 600))) '(-101 -188 318 424 605 6 7 8 9 10) "add-pointwise-list test ep5")
+   (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-2 10 15 20))) '(-4 20 30 40 0 0 7 8 9 10) "add-pointwise-list test ep6")
 
    ;-------------------stream-for-n-steps tests------------------------------
 
-  (check-equal? (stream-for-n-steps nat-num-stream 10)
-                 '(0 1 2 3 4 5 6 7 8 9)
-                 "stream-for-n-steps test")
+  (check-equal? (stream-for-n-steps nat-num-stream 10) '(0 1 2 3 4 5 6 7 8 9) "stream-for-n-steps test")
                  
   ; (epwr tests)
   (check-equal? (stream-for-n-steps nat-num-stream 15) '(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14) "nat-num-stream test ep1")
@@ -92,9 +84,7 @@
 
   ;--------------------------fibo-stream tests------------------------------
   
-  (check-equal? (stream-for-n-steps fibo-stream 10)
-                 '(0 1 1 2 3 5 8 13 21 34)
-                 "fibo-stream test")
+  (check-equal? (stream-for-n-steps fibo-stream 10) '(0 1 1 2 3 5 8 13 21 34) "fibo-stream test")
                  
   ; (epwr tests)
   (check-equal? (stream-for-n-steps fibo-stream 15) '(0 1 1 2 3 5 8 13 21 34 55 89 144 233 377) "fibo-stream test ep2")
@@ -104,10 +94,7 @@
 
   ;--------------------------filter-stream tests------------------------------
   
-  (check-equal? (stream-for-n-steps 
-                  (filter-stream (lambda (i) (> i 5)) nat-num-stream) 5)
-                 '(6 7 8 9 10)
-                 "filter stream test")
+  (check-equal? (stream-for-n-steps (filter-stream (lambda (i) (> i 5)) nat-num-stream) 5) '(6 7 8 9 10) "filter stream test")
                  
   ; (epwr tests)
   (check-equal? (stream-for-n-steps (filter-stream (lambda (i) (integer? (/ i 3))) nat-num-stream) 5) '(0 3 6 9 12) "filter nat-num-stream test")
@@ -116,37 +103,28 @@
 
   ;-----------------------palyndromic-numbers tests---------------------------
   
-  (check-equal? (stream-for-n-steps palyndromic-numbers 20)
-                 '(0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101)
-                 "palyndromic-numbers test")
+  (check-equal? (stream-for-n-steps palyndromic-numbers 20) '(0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101) "palyndromic-numbers test")
                  
   ; (epwr tests)
   (check-equal? (stream-for-n-steps palyndromic-numbers 15) '(0 1 2 3 4 5 6 7 8 9 11 22 33 44 55) "palyndromic-numbers test ep1")
 
   ;-------------------------------squares tests------------------------------
   
-   (check-equal? (stream-for-n-steps squares 5)
-                 '(25 49 81 121 169)
-                 "stream defined using a macro. only tests is return value")
+  (check-equal? (stream-for-n-steps squares 5) '(25 49 81 121 169) "stream defined using a macro. only tests is return value")
 
   ;--------------------------vector-assoc tests------------------------------
    
-   (check-equal? (vector-assoc 4 (vector (cons 2 1) (cons 3 1) (cons 4 1) (cons 5 1)))
-                 (cons 4 1)
-                 "vector-assoc test")
-   ;(Greay tests)
-   (check-equal? (vector-assoc 4 (vector #f #f #f #f #f #f)) #f "vector-assoc test")
+  (check-equal? (vector-assoc 4 (vector (cons 2 1) (cons 3 1) (cons 4 1) (cons 5 1))) (cons 4 1) "vector-assoc test")
+   
+  ;(Greay tests)
+  (check-equal? (vector-assoc 4 (vector #f #f #f #f #f #f)) #f "vector-assoc test")
 
   ;--------------------------cached-assoc tests------------------------------
    
-   ;; note that the following test tests functionality but not performance
-   (check-equal?
-    (let
-        [(cache (cached-assoc  (list (cons 1 2) (cons 3 4)) 3) )]
-           (cache 3))
-    (cons 3 4)
-    "cached-assoc test")   
-   ))
+  ;; note that the following test tests functionality but not performance
+  (check-equal? (let [(cache (cached-assoc  (list (cons 1 2) (cons 3 4)) 3) )] (cache 3)) (cons 3 4)  "cached-assoc test")
+  
+  ))
 
 (require rackunit/text-ui)
 ;; runs the test
