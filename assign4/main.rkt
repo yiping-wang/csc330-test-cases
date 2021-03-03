ad8dc3d76ba6668bc742ad252efc4a7bea1b363b
Yiping Wang <yipingwang@uvic.ca>
2021-03-03 13:13:26 -0800

    remove one invalid test;

--- assign4/main.rkt
+++ assign4/main.rkt
@@ -38,9 +38,6 @@
    (check-equal? (add-pointwise '() '(1 2)) (list 1 2) "add-pointwise test")
    (check-equal? (add-pointwise '(3 4) '(1 2)) (list 4 6) "add-pointwise test")
    (check-equal? (add-pointwise '(3.4 4) '(1 2.2)) (list 4.4 6.2) "add-pointwise test real")
-
-   ;(turtle tests)
-   (check-equal? (with-handlers ([exn:fail? (lambda (exn) (exn-message exn))]) (add-pointwise '(1.2 "racket") '(1 2))) "illegal parameter" "add-pointwise test")
    
    ; (epwr tests)
    (check-equal? (add-pointwise '(1 2 3) '(4 2)) (list 5 4 3) "add-pointwise test")
